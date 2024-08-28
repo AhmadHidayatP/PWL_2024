80df38b4f981d8efbd43accb13e4f5daa7e86d4e
Ahmad Hidayat <ahmadhidayatpermana202@gmail.com>
2024-08-28 10:07:10 +0700

    Edit Tampilan KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,82 @@
 <!DOCTYPE html>
-    <html lang="{{ str_replace( app()->getLocale()) }}">
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-
-    <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
-        <h1>Semangat, Belajar Laravel 10/h3>
-    </body>
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KTP Hidayat</title>
+</head>
+<body>
+    <h1>Selamat Datang</h1><br>
+    <div class="content">
+    <table>
+        <tr>
+            <th colspan="5" style="font-size: large;font-weight: bold;padding-bottom: 1px;">PROVINSI JAWA TIMUR</th>
+        </tr>
+        <tr>
+            <th colspan="5" style="font-size: large;font-weight: bold;padding-bottom: 5px;">KOTA MALANG</th>
+        </tr>
+        <tr>
+            <td style="font-size: large;font-weight: bold;padding-bottom: 5px;">NIK</td>
+            <td style="font-size: large;font-weight: bold;padding-bottom: 5px;">:35730*****</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>:AHMAD HIDAYAT PERMANA</td>
+        </tr>
+        <tr>
+            <td>Tempat/Tgl Lahir</td>
+            <td>:MALANG, **-**-****</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>:LAKI-LAKI</td>
+                <td>Gol. Darah :</td>
+                <td>A</td>
+        </tr>
+        <tr>
+            <td>Alamat </td>
+            <td>:JL. ****</td>
+        </tr>
+            <tr>
+                <td style="padding-left: 25px;">RT/RW</td>
+                <td>:****</td>
+            </tr>
+            <tr>
+                <td style="padding-left: 25px;">Kel/Desa</td>
+                <td>:****</td>
+            </tr>
+            <tr>
+                <td style="padding-left: 25px;">Kecamatan</td>
+                <td>"****</td>
+            </tr>
+        <tr>
+            <td>Agama</td>
+            <td>:ISLAM</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>:BELUM KAWIN</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>:PELAJAR/MAHASISWA</td>
+        </tr>
+        <tr>
+            <td>Kewarganegaraan</td>
+            <td>:WNI</td>
+            <td colspan="2"></td>
+            <td style="align-items: center;justify-content: center;display: flex; padding-bottom: 5px;">KOTA MALANG</td>
+        </tr>
+        <tr>
+            <td>Berlaku Hingga</td>
+            <td>:SEUMUR HIDUP</td>
+            <td colspan="2"></td>
+            <td style="align-items: center;justify-content: center;display: flex;">02-01-2020</td>
+        </tr>
+        <tr>
+            <td colspan=4 style="padding-bottom: 50px;"></td>
+        </tr>
+    </table>        
+</div>
+</body>
 </html>
